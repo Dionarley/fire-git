a4c6dfadf6f506a8e45ad5d5053a4adb6813ed9c
Dionarley <dionarley@gmail.com>
2025-08-31 11:12:52 -0300

    Update firegit.sh

--- firegit.sh
+++ firegit.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+ascii_art=
+' _____ _             ____ _ _   
+|  ___(_)_ __ ___   / ___(_) |_ 
+| |_  | | '__/ _ \ | |  _| | __|
+|  _| | | | |  __/ | |_| | | |_ 
+|_|   |_|_|  \___|  \____|_|\__|
+'
+echo -e " ascii_art "
+
 echo "🚀 Iniciando instalador do Git Config Helper..."
 
 # === Detectar SO e gerenciador de pacotes ===
